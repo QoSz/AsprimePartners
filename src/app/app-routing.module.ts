8ea842e9235c047dca6ba98fff75a7bc4a402642
Yash Shah <yashshah1282@gmail.com>
2023-01-11 16:55:38 +0300

    Added dashboard child components to app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CustomerSalesComponent } from './components/dashboard/components/customer-sales/customer-sales.component';
+import { UploadDocumentComponent } from './components/dashboard/components/upload-document/upload-document.component';
+import { ChangePasswordComponent } from './components/dashboard/components/change-password/change-password.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'dashboard', component: DashboardComponent, children: [
+    { path: 'customer-sales', component: CustomerSalesComponent },
+    { path: 'upload-documents', component: UploadDocumentComponent },
+    { path: 'change-password', component: ChangePasswordComponent }
+  ]}
 ];
 
 @NgModule({
