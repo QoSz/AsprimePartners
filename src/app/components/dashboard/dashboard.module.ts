23eeafa725fdaa33c4d713f9c513ad9fbf8cffdb
Yash Shah <yashshah1282@gmail.com>
2023-01-11 16:49:49 +0300

    Added components to dashboard module

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { CustomerSalesComponent } from './components/customer-sales/customer-sales.component';
+import { UploadDocumentComponent } from './components/upload-document/upload-document.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    CustomerSalesComponent,
+    UploadDocumentComponent,
+    ChangePasswordComponent
   ],
   imports: [
     CommonModule,
