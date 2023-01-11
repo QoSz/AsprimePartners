b2a0dec3d2f3ffc3bc79dad97713f95d3c059a9d
Yash Shah <yashshah1282@gmail.com>
2023-01-11 16:11:40 +0300

    Got bootstrap working

---
+++ src/app/dashboard/components/customer-sales/customer-sales.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-sales',
+  templateUrl: './customer-sales.component.html',
+  styleUrls: ['./customer-sales.component.scss']
+})
+export class CustomerSalesComponent {
+
+}
