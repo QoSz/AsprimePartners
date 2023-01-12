8c95b3bcf66778f8ccf47b3c8cf1b93eb9939bb4
Yash Shah <yashshah1282@gmail.com>
2023-01-12 16:58:55 +0300

    Partially completed customer sales page. Need to work on getting the title working

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  title: string = "Customer Sales"
+
+  constructor(private location: Location  ) {}
+
+  ngOnInit() {
+    const fullUrl = this.location.path().replace('/dashboard/', '');
+    if (fullUrl == "customer-sales") {
+      this.title = "Customer Sales";
+    } else if (fullUrl == "upload-documents") {
+      this.title = "Upload Document";
+    } else if (fullUrl == "change-password") {
+      this.title = "Change Password";
+    } else {
+      this.title = "Customer Sales";  
+    }
+  }
 
 }
